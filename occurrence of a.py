edfa2d5d67994cfcaafa7670fed9cfaf72b0d951
Aswathy-CA <144875341+Aswathy-CA@users.noreply.github.com>
2023-11-15 11:49:41 +0530

    Update occurrence of a.py

--- occurrence of a.py
+++ occurrence of a.py
@@ -1,9 +1,12 @@
-list=[]
-name=input("Enter the first name :")
-list.append(name)
-count=0
-for i in list[0]:
+b=[]
+n=int(input('Enter number of names:'))
+for i in range(n):
+    name=input('Enter the first name:')
+    b.append(name)
+print(b)
+a=str(b)
+count=0   
+for i in a:
     if i=='a':
         count=count+1
-print("Count of a in",list[0],":",str(count))
-    
+print('Count of a is',count)
