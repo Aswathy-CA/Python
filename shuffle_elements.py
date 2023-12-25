505b57e76fe99c58e9ecd949a6f2ae14f2c4e789
Aswathy-CA <144875341+Aswathy-CA@users.noreply.github.com>
2023-12-25 21:02:21 +0530

    shuffle_elements.py

--- shuffle_elements.py
+++ shuffle_elements.py
@@ -1,5 +1,5 @@
 import random
-l=["ammu","appu","achu","anu","aju"]
-print("original list=",l)
-a=random.shuffle(l)
-print("shuffle list=",l)
+original_list=[1,"apple",3.14,"banana",42,"orange"]
+shuffled_list=random.sample(original_list,len(original_list))
+print("original list:",original_list)
+print("shuffled list:",shuffled_list)
