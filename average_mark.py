0b8bcaf992fc13cdf1f3793b15c7f0ca00d8a37c
Aswathy-CA <144875341+Aswathy-CA@users.noreply.github.com>
2023-12-25 20:52:46 +0530

    Update average_mark.py

--- average_mark.py
+++ average_mark.py
@@ -4,6 +4,6 @@ mark3=int(input("Enter mark of subject3:"))
 mark4=int(input("Enter mark of subject4:"))
 mark5=int(input("Enter mark of subject5:"))
 sum=mark1+mark2+mark3+mark4+mark5
-avg=float((sum)/4)
+avg=float((sum)/5)
 print("Sum is ",sum)
 print("Average is ",avg)
